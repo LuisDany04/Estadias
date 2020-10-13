4f7ae626397d2c4c5210bd6a63aabc511063facd
Daniel Gonzalez <dany_slender04@hotmail.com>
2020-10-13 11:57:05 -0500

    Creation

---
+++ enemGFX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class enemGFX : MonoBehaviour{
+    public AIPath aiPath;
+    Vector3 Flip;
+    float FlipX;
+    // Start is called before the first frame update
+    void Start(){
+
+        Flip = transform.localScale;
+        FlipX = Flip.x;
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        if (aiPath.desiredVelocity.x >= 0.01f) {
+            Flip.x = -FlipX;
+        }
+        else if (aiPath.desiredVelocity.x <= -0.01f) {
+            Flip.x = FlipX;
+        }
+        transform.localScale = Flip;
+    }
+}
---
+++ playerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour{
+
+    public float speed;
+    public float diagonalSpeed;
+    Vector3 newScale;
+    float newScaleX;
+    bool InputEnabled = true;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start(){
+        anim = GetComponent<Animator>();
+        newScale = transform.localScale;
+        newScaleX = newScale.x;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (InputEnabled) {
+
+            float inputX = Input.GetAxisRaw("Horizontal");
+            float inputY = Input.GetAxisRaw("Vertical");
+
+            //Movement
+            if (inputX != 0 && inputY != 0) {
+                transform.Translate(inputX * (speed-1) * Time.deltaTime, 0f, 0f);
+                transform.Translate(0f, inputY * (speed-1) * Time.deltaTime, 0f);
+            }
+            else {
+                transform.Translate(inputX * speed * Time.deltaTime, 0f, 0f);
+                transform.Translate(0f, inputY * speed * Time.deltaTime, 0f);
+            }
+
+            //Flip
+            if (inputX > 0) newScale.x = newScaleX;
+            if (inputX < 0) newScale.x = -newScaleX;
+            transform.localScale = newScale;
+
+            //Animation detection
+            if (inputX != 0 || inputY != 0) {
+                anim.SetBool("Moving", true);
+            }
+            else if (inputX == 0 && inputY == 0) {
+                anim.SetBool("Moving", false);
+            }
+        }
+
+    }
+}
